50485ebb59b4745df4886d92f62bce605c7564c4
trihq <triquoc.dev@gmail.com>
2026-02-21 00:52:57 +0700

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
   }
   
   await app.listen(port);
